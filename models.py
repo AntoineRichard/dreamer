aedc3ed5df8246de8bed1b7b298a918aaefaedcb
AntoineRichard <antoine.richard@gatech.edu>
2020-10-11 14:59:50 +0200

    added 1D CNNs for encoder and decoder

--- models.py
+++ models.py
@@ -6,6 +6,18 @@ from tensorflow.keras.mixed_precision import experimental as prec
 
 import tools
 
+def Conv1DTranspose(input_tensor, filters, kernel_size, strides=2, padding='same', activation='relu'):
+    """
+        input_tensor: tensor, with the shape (batch_size, time_steps, dims)
+        filters: int, output dimension, i.e. the output tensor will have the shape of (batch_size, time_steps, filters)
+        kernel_size: int, size of the convolution kernel
+        strides: int, convolution step size
+        padding: 'same' | 'valid'
+    """
+    x = tfkl.Lambda(lambda x: tf.expand_dims(x, axis=2))(input_tensor)
+    x = tfkl.Conv2DTranspose(filters=filters, kernel_size=(kernel_size, 1), strides=(strides, 1), padding=padding, activation=activation)(x)
+    x = tfkl.Lambda(lambda x: tf.squeeze(x, axis=2))(x)
+    return x
 
 class RSSM(tools.Module):
 
@@ -27,16 +39,16 @@ class RSSM(tools.Module):
 
   @tf.function
   def observe(self, embed, action, state=None):
-    if state is None:
+    if state is None: # No state --> set state to 0
       state = self.initial(tf.shape(action)[0])
-    embed = tf.transpose(embed, [1, 0, 2])
-    action = tf.transpose(action, [1, 0, 2])
+    embed = tf.transpose(embed, [1, 0, 2]) # BS,Length,Feat --> Length, BS, feat
+    action = tf.transpose(action, [1, 0, 2]) # BS,Length,Feat --> Length, BS, feat
     post, prior = tools.static_scan(
-        lambda prev, inputs: self.obs_step(prev[0], *inputs),
-        (action, embed), (state, state))
-    post = {k: tf.transpose(v, [1, 0, 2]) for k, v in post.items()}
-    prior = {k: tf.transpose(v, [1, 0, 2]) for k, v in prior.items()}
-    return post, prior
+        lambda prev, inputs: self.obs_step(prev[0], *inputs), # transforms the data to state, action, embed
+        (action, embed), (state, state)) # Applies obs_step to each element of the sequence in a batch fashion
+    post = {k: tf.transpose(v, [1, 0, 2]) for k, v in post.items()} # Undo previous transpose
+    prior = {k: tf.transpose(v, [1, 0, 2]) for k, v in prior.items()} # Undo previous transpose
+    return post, prior #post : (state,state), prior:
 
   @tf.function
   def imagine(self, action, state=None):
@@ -80,7 +92,6 @@ class RSSM(tools.Module):
     prior = {'mean': mean, 'std': std, 'stoch': stoch, 'deter': deter}
     return prior
 
-
 class ConvEncoder(tools.Module):
 
   def __init__(self, depth=32, act=tf.nn.relu):
@@ -97,6 +108,22 @@ class ConvEncoder(tools.Module):
     shape = tf.concat([tf.shape(obs['image'])[:-3], [32 * self._depth]], 0)
     return tf.reshape(x, shape)
 
+class LaserConvEncoder(tools.Module):
+
+  def __init__(self, depth=32, act=tf.nn.relu):
+    self._act = act
+    self._depth = depth
+
+  def __call__(self, obs):
+    kwargs = dict(strides=2, activation=self._act)
+    x = tf.reshape(obs['laser'], (-1,) + tuple(obs['laser'].shape[-2:]))
+    x = self.get('h1', tfkl.Conv1D, 1 * self._depth, 6, **kwargs)(x)
+    x = self.get('h2', tfkl.Conv1D, 2 * self._depth, 6, **kwargs)(x)
+    x = self.get('h3', tfkl.Conv1D, 3 * self._depth, 6, **kwargs)(x)
+    x = self.get('h4', tfkl.Conv1D, 4 * self._depth, 6, **kwargs)(x)
+    x = self.get('h5', tfkl.Conv1D, 8 * self._depth, 6, **kwargs)(x)
+    shape = tf.concat([tf.shape(obs['laser'])[:-2], [32*self._depth]], 0)
+    return tf.reshape(x, shape)
 
 class ConvDecoder(tools.Module):
 
@@ -113,9 +140,29 @@ class ConvDecoder(tools.Module):
     x = self.get('h3', tfkl.Conv2DTranspose, 2 * self._depth, 5, **kwargs)(x)
     x = self.get('h4', tfkl.Conv2DTranspose, 1 * self._depth, 6, **kwargs)(x)
     x = self.get('h5', tfkl.Conv2DTranspose, self._shape[-1], 6, strides=2)(x)
+
     mean = tf.reshape(x, tf.concat([tf.shape(features)[:-1], self._shape], 0))
     return tfd.Independent(tfd.Normal(mean, 1), len(self._shape))
 
+class LaserConvDecoder(tools.Module):
+
+  def __init__(self, depth=32, act=tf.nn.relu, shape=(256, 1)):
+    self._act = act
+    self._depth = depth
+    self._shape = shape
+
+  def __call__(self, features):
+    kwargs = dict(strides=(2,1), activation=self._act, padding='same')
+    kwargs2 = dict(strides=(4,1), activation=self._act, padding='same')
+    x = self.get('h1', tfkl.Dense, 32 * self._depth, None)(features)
+    x = tf.reshape(x, [-1, 1, 1, 32 * self._depth])
+    x = self.get('h2', tfkl.Conv2DTranspose, 8 * self._depth, (5,1), **kwargs2)(x)
+    x = self.get('h3', tfkl.Conv2DTranspose, 4 * self._depth, (5,1), **kwargs2)(x)
+    x = self.get('h4', tfkl.Conv2DTranspose, 2 * self._depth, (5,1), **kwargs2)(x)
+    x = self.get('h5', tfkl.Conv2DTranspose, 1 * self._depth, (5,1), **kwargs)(x)
+    x = self.get('h6', tfkl.Conv2DTranspose, self._shape[-1], (5,1), strides=(2,1), padding='same')(x)
+    mean = tf.reshape(x, tf.concat([tf.shape(features)[:-1], self._shape], 0))
+    return tfd.Independent(tfd.Normal(mean, 1), len(self._shape))
 
 class DenseDecoder(tools.Module):
 
